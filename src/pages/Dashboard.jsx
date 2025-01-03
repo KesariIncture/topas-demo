2485d3e7ea3baabf0eb75f9a013cda68606c5e9c
KesariIncture <kesarinandan.patro@incture.com>
2025-01-03 17:15:52 +0530

    Add marquee for important announcements in Dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,10 @@
 import { Typography, Box, IconButton } from "@mui/material";
 import Header from "../components/Header";
 import Sidebar from "../components/Sidebar";
-import { Home } from "@mui/icons-material";
+import { Home, Report } from "@mui/icons-material";
+import Marquee from "react-fast-marquee";
+import { Link } from "react-router";
+
 
 function Dashboard() {
   const c1 = "#00518D";
@@ -11,26 +14,45 @@ function Dashboard() {
       <Header />
       <Sidebar />
 
-      <Box
-        sx={{
-          margin: "5rem 0 0 5rem",
-          display: "flex",
-          justifyContent: "space-between",
-        }}
-      >
-        <Typography variant="h6" sx={{ color: c1}}>
-          Welcome, Kesari Nandan
-        </Typography>
-        <IconButton
+      <Box sx={{ margin: "5rem 0 0 5rem" }}>
+        <Marquee>
+          <Box sx={{ display: "flex", gap: "7px" }}>
+            <Typography
+              sx={{
+                color: "red",
+                fontWeight: "bold",
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
+              <Report fontSize="small" />
+              Important:
+            </Typography>
+            <Typography sx = {{fontWeight: "bold"}}>
+              Risk Management and Seven Keys have been moved to <Link>Promise PORTAL</Link>
+            </Typography>
+          </Box>
+        </Marquee>
+        <Box
           sx={{
-            color: c1,
-            "&:hover": {
-              backgroundColor: "rgba(0, 81, 141, 0.1)",
-            },
+            display: "flex",
+            justifyContent: "space-between",
           }}
         >
-          <Home />
-        </IconButton>
+          <Typography variant="h6" sx={{ color: c1 }}>
+            Welcome, Kesari Nandan
+          </Typography>
+          <IconButton
+            sx={{
+              color: c1,
+              "&:hover": {
+                backgroundColor: "rgba(0, 81, 141, 0.1)",
+              },
+            }}
+          >
+            <Home />
+          </IconButton>
+        </Box>
       </Box>
     </>
   );
