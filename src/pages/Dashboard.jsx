f6db0c5cfd717afb38f1c284a36b772136447efd
KesariIncture <kesarinandan.patro@incture.com>
2025-01-13 11:11:16 +0530

    Add react-hook-form dependency and clean up Dashboard imports

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -15,12 +15,7 @@ import Marquee from "react-fast-marquee";
 import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
+
 
 function Dashboard() {
   const c1 = "#00518D";
